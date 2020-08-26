63f67016bd6e17752f8d0ff06cf96034ff3bf583
ken stephens <kenstephens33@gmail.com>
2020-08-25 21:19:11 -0400

    code

--- cowsay_app/views.py
+++ cowsay_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from cowsay_app.forms import AddTxtForm
 from cowsay_app.models import Txt
+from subprocess import check_output
 
 # Create your views here.
 
@@ -10,7 +11,10 @@ def index_view(request):
     if request.method == 'POST': 
             form = AddTxtForm(request.POST)
             if form.is_valid():
+                form.save(commit=False)
+                txt = form.cleaned_data.get('user_input')
                 form.save()
-                form = AddTxtForm()
+                return render(request, 'index.html', {'form': form, 'output': check_output(['cowsay', txt]).decode('utf-8')})
+    form = AddTxtForm()
     return render(request, 'index.html', {'form': form})
     
